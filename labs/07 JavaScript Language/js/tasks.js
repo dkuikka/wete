50a421f4913f0fc35729de521125f8093edd7a6d
Dani Kuikka <dani.kuikka@metropolia.fi>
2019-04-03 13:35:44 +0300

    lab7 done

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -4,6 +4,29 @@
 // Need a global variable:
 var tasks = []; 
 
+function removeDuplicates() {
+    'use strict'
+    var i = 0;
+    var j = 1;
+    console.log("remove duplicates");
+    for(i=0; i < tasks.length; i++) {
+        for(j=1; j < tasks.length; j++) {
+            console.log(tasks[j] + tasks[j])
+        if (tasks[i] == tasks[j]) {
+            tasks.splice(i, 1);
+        }
+        }
+    }
+    // Update the page:
+    var message = '';
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+}
+
 // Function called when the form is submitted.
 // Function adds a task to the global array.
 function addTask() {
@@ -42,5 +65,6 @@ function addTask() {
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    //document.getElementById('removeduplicates').onclick = removeDuplicates();
 } // End of init() function.
 window.onload = init;
